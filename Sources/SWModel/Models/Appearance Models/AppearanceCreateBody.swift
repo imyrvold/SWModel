8aace3f804b6e2e1f17e98a19544573adf5842cd
Ivan C Myrvold <ivan@myrvold.org>
2020-10-04 12:57:19 +0200

    remove custom encodable of AppearanceCreateBody

--- Sources/SWModel/Models/Appearance Models/AppearanceCreateBody.swift
+++ Sources/SWModel/Models/Appearance Models/AppearanceCreateBody.swift
@@ -20,47 +20,4 @@ public struct AppearanceCreateBody {
     public var isExpanded: Bool?
 }
 
-extension AppearanceCreateBody: Encodable {
-    public enum CodingKeys: String, CodingKey {
-        case name
-        case prefix
-        case suffix
-        case size
-        case holderSize
-        case textSize
-        case selectionColor
-        case backgroundColor
-        case isExpanded
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-
-        try container.encode(self.name, forKey: .name)
-        if let prefix = self.prefix {
-            try container.encode(prefix, forKey: .prefix)
-        }
-        if let suffix = self.suffix {
-            try container.encode(suffix, forKey: .suffix)
-        }
-        if let size = self.size {
-            try container.encode(size, forKey: .size)
-        }
-        if let holderSize = self.holderSize {
-            try container.encode(holderSize, forKey: .holderSize)
-        }
-        if let textSize = self.textSize {
-            try container.encode(textSize, forKey: .textSize)
-        }
-        if let selectionColor = self.selectionColor {
-            try container.encode(selectionColor, forKey: .selectionColor)
-        }
-        if let backgroundColor = self.backgroundColor {
-            try container.encode(backgroundColor, forKey: .backgroundColor)
-        }
-        if let isExpanded = self.isExpanded {
-            try container.encode(isExpanded, forKey: .isExpanded)
-        }
-    }
-
-}
+extension AppearanceCreateBody: Encodable {}
