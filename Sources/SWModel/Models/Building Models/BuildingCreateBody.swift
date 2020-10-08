cf080ec587d7582d8b7da78403b02afd85248d87
Ivan C Myrvold <ivan@myrvold.org>
2020-10-08 15:47:42 +0200

    use ObjectId in BuildingCreateBody

--- Sources/SWModel/Models/Building Models/BuildingCreateBody.swift
+++ Sources/SWModel/Models/Building Models/BuildingCreateBody.swift
@@ -14,15 +14,15 @@ public struct BuildingCreateBody: Encodable {
     let address: String?
     let imageUrl: String?
     let index: String?
-    let pages: [String]?
-    let menus: [String]?
+    let pages: [ObjectId]?
+    let menus: [ObjectId]?
     let links: [Link]?
     let position: Int?
     let claim: ObjectId
-    let company: String?
+    let company: ObjectId?
     
-    public init(name: String, address: String?, imageUrl: String?, index: String?, pages: [String]?, menus: [String]?, links: [Link]?, position: Int?,
-        claim: ObjectId, company: String?) {
+    public init(name: String, address: String?, imageUrl: String?, index: String?, pages: [ObjectId]?, menus: [ObjectId]?, links: [Link]?, position: Int?,
+        claim: ObjectId, company: ObjectId?) {
         self.name = name
         self.address = address
         self.imageUrl = imageUrl
