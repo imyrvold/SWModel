2298bf312766be58511932acdf2c67b89767c3f0
Ivan C Myrvold <ivan@myrvold.org>
2020-10-18 09:27:16 +0200

    added init method to AppearanceResponse

--- Sources/SWModel/Models/Appearance Models/AppearanceResponse.swift
+++ Sources/SWModel/Models/Appearance Models/AppearanceResponse.swift
@@ -9,7 +9,7 @@ import Foundation
 import BSON
 
 public struct AppearanceResponse: Codable {
-    public let id: ObjectId
+    public let id: ObjectId?
     public let name: String
     public var prefix: String?
     public var suffix: String?
@@ -19,7 +19,27 @@ public struct AppearanceResponse: Codable {
     public var selectionColor: String?
     public var backgroundColor: String?
     public var isExpanded: Bool?
-    public let createdAt: String
-    public let updatedAt: String
-    public let deletedAt: String?
+    public let createdAt: Date
+    public let updatedAt: Date
+    public let deletedAt: Date?
+    
+    init(id: ObjectId?, name: String, prefix: String?, suffix: String?, size: Int?, holderSize: Int?, textSize: Double?, selectionColor: String?, backgroundColor: String?, isExpanded: Bool?, createdAt: Date, updatedAt: Date, deletedAt: Date?) {
+        self.id = id
+        self.name = name
+        self.prefix = prefix
+        self.suffix = suffix
+        self.size = size
+        self.holderSize = holderSize
+        self.textSize = textSize
+        self.selectionColor = selectionColor
+        self.backgroundColor = backgroundColor
+        self.isExpanded = isExpanded
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+        self.deletedAt = deletedAt
+    }
+    
+    public init(from appearance: Appearance) {
+        self.init(id: appearance.id, name: appearance.name, prefix: appearance.prefix, suffix: appearance.suffix, size: appearance.size, holderSize: appearance.holderSize, textSize: appearance.textSize, selectionColor: appearance.selectionColor, backgroundColor: appearance.backgroundColor, isExpanded: appearance.isExpanded, createdAt: appearance.createdAt, updatedAt: appearance.updatedAt, deletedAt: appearance.deletedAt)
+    }
 }
