5e6c6b45a690511b0a9c8944a3de7095f6cfef5b
Ivan C Myrvold <ivan@myrvold.org>
2020-10-24 16:21:56 +0200

    init for decoding AppearanceResponse

--- Sources/SWModel/Models/Appearance Models/AppearanceResponse.swift
+++ Sources/SWModel/Models/Appearance Models/AppearanceResponse.swift
@@ -39,6 +39,24 @@ public struct AppearanceResponse: Codable {
         self.deletedAt = deletedAt
     }
     
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try? container.decode(ObjectId.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.prefix = try? container.decode(String.self, forKey: .prefix)
+        self.suffix = try? container.decode(String.self, forKey: .suffix)
+        self.size = try? container.decode(Int.self, forKey: .size)
+        self.holderSize = try? container.decode(Int.self, forKey: .holderSize)
+        self.textSize = try? container.decode(Double.self, forKey: .textSize)
+        self.selectionColor = try? container.decode(String.self, forKey: .selectionColor)
+        self.backgroundColor = try? container.decode(String.self, forKey: .backgroundColor)
+        self.isExpanded = try? container.decode(Bool.self, forKey: .isExpanded)
+        self.createdAt = try? container.decode(Date.self, forKey: .createdAt)
+        self.updatedAt = try? container.decode(Date.self, forKey: .updatedAt)
+        self.deletedAt = try? container.decode(Date.self, forKey: .deletedAt)
+    }
+
+    
     public init(from appearance: Appearance) {
         self.init(id: appearance.id, name: appearance.name, prefix: appearance.prefix, suffix: appearance.suffix, size: appearance.size, holderSize: appearance.holderSize, textSize: appearance.textSize, selectionColor: appearance.selectionColor, backgroundColor: appearance.backgroundColor, isExpanded: appearance.isExpanded, createdAt: appearance.createdAt, updatedAt: appearance.updatedAt, deletedAt: appearance.deletedAt)
     }
