9bc516868eb81ac9bfc97910996f0362094841ce
Ivan Myrvold <ivan.myrvold@abax.no>
2020-12-04 12:35:22 +0100

    update to latest OS versions

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SWModel",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
+    platforms: [.macOS(.v11), .iOS(.v14), .tvOS(.v14), .watchOS(.v7)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
