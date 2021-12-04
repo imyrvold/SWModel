ed2e55e3d8ce369de8ccde6d4d1f027d4ea382d4
Ivan C Myrvold <ivan@myrvold.org>
2021-12-04 16:51:36 +0100

    adding json function to Claim

--- Sources/SWModel/Models/Claim Models/ClaimResponse.swift
+++ Sources/SWModel/Models/Claim Models/ClaimResponse.swift
@@ -15,3 +15,25 @@ public struct ClaimResponse: Codable {
     public let updatedAt: String
     public let deletedAt: String?
 }
+
+public extension ClaimResponse {
+    struct ClaimJson: Encodable {
+        let id: String
+        let name: String
+        let createdAt: String
+        let updatedAt: String
+        let deletedAt: String?
+    }
+
+    static func json(from response: [ClaimResponse]) throws -> String {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        let claimsOutput = response.map { ClaimJson(id: $0.id.hexString, name: $0.name, createdAt: $0.createdAt, updatedAt: $0.updatedAt, deletedAt: $0.deletedAt) }
+        
+        let claimsData = try encoder.encode(claimsOutput)
+        if let json = String(data: claimsData, encoding: .utf8) {
+            return json
+        }
+        return "Couldn't convert response to JSON"
+    }
+}
