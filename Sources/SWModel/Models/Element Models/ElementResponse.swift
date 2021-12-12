1be4aef8c5b80f7e7bbd5387f4d6b85c4c64783d
Ivan C Myrvold <ivan@myrvold.org>
2021-12-12 09:15:07 +0100

    added ElementResponseLegacy

--- Sources/SWModel/Models/Element Models/ElementResponse.swift
+++ Sources/SWModel/Models/Element Models/ElementResponse.swift
@@ -17,14 +17,9 @@ public struct ElementResponse: Codable {
 }
 
 public extension ElementResponse {
-//    static func elementResponse(from response: ElementResponse) -> ElementResponse {
-//        Element(id: response.id.hexString, name: response.name, claim: response.claim, createdAt: response.createdAt, updatedAt: response.updatedAt, deletedAt: response.deletedAt)
-//    }
-
     static func json(from response: [ElementResponse]) throws -> String {
         let encoder = JSONEncoder()
         encoder.outputFormatting = .prettyPrinted
-//        let elementsOutput = response.map { Self.elementResponse(from: $0) }
         
         let elementsData = try encoder.encode(response)
         if let json = String(data: elementsData, encoding: .utf8) {
@@ -36,7 +31,6 @@ public extension ElementResponse {
     static func json(from response: ElementResponse) throws -> String {
         let encoder = JSONEncoder()
         encoder.outputFormatting = .prettyPrinted
-//        let elementOutput = Self.elementResponse(from: response)
         
         let elementsData = try encoder.encode(response)
         if let json = String(data: elementsData, encoding: .utf8) {
---
+++ Sources/SWModel/Models/Element Models/ElementResponseLegacy.swift
@@ -0,0 +1,81 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ivan C Myrvold on 12/12/2021.
+//
+
+import Foundation
+import BSON
+
+public struct ElementResponseLegacy: Codable {
+    public let id: ObjectId
+    public let name: String
+    public let type: ElementType
+    public var data: ElementData?
+    public var claim: Claim
+    public let createdOn: String
+    public let updatedOn: String
+    public let deletedOn: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case name
+        case type
+        case claim
+        case data
+        case createdOn
+        case updatedOn
+        case deletedOn
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decode(ObjectId.self, forKey: .id)
+        name = try values.decode(String.self, forKey: .name)
+        type = try values.decode(ElementType.self, forKey: .type)
+        claim = try values.decode(Claim.self, forKey: .claim)
+        createdOn = try values.decode(String.self, forKey: .createdOn)
+        updatedOn = try values.decode(String.self, forKey: .updatedOn)
+        deletedOn = try? values.decode(String.self, forKey: .deletedOn)
+        switch type {
+        case .tagGroup:
+            data = try .tagGroup(values.decode(TagGroupData.self, forKey: .data))
+        case .tagList:
+            data = try .tagList(values.decode(TagListData.self, forKey: .data))
+        case .alarmList:
+            data = try .alarmList(values.decode(AlarmListData.self, forKey: .data))
+        case .alarmLogList:
+            data = try .alarmLogList(values.decode(AlarmLogListData.self, forKey: .data))
+        case .iframe:
+            data = try .iframe(values.decode(IframeData.self, forKey: .data))
+        case .image:
+            data = try .image(values.decode(ImageData.self, forKey: .data))
+        }
+    }
+}
+
+public extension ElementResponseLegacy {
+    static func json(from response: [ElementResponseLegacy]) throws -> String {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        
+        let elementsData = try encoder.encode(response)
+        if let json = String(data: elementsData, encoding: .utf8) {
+            return json
+        }
+        return "Couldn't convert response to JSON"
+    }
+
+    static func json(from response: ElementResponseLegacy) throws -> String {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        
+        let elementsData = try encoder.encode(response)
+        if let json = String(data: elementsData, encoding: .utf8) {
+            return json
+        }
+        return "Couldn't convert response to JSON"
+    }
+
+}
