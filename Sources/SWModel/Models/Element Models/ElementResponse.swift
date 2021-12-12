1acffe643bad22a3e5a3ae7f8a7719df174b4ed5
Ivan C Myrvold <ivan@myrvold.org>
2021-12-12 08:11:54 +0100

    adding json static func extension to ElementResponse

--- Sources/SWModel/Models/Element Models/ElementResponse.swift
+++ Sources/SWModel/Models/Element Models/ElementResponse.swift
@@ -15,3 +15,34 @@ public struct ElementResponse: Codable {
     public var data: ElementData?
     public var claim: String?
 }
+
+public extension ElementResponse {
+//    static func elementResponse(from response: ElementResponse) -> ElementResponse {
+//        Element(id: response.id.hexString, name: response.name, claim: response.claim, createdAt: response.createdAt, updatedAt: response.updatedAt, deletedAt: response.deletedAt)
+//    }
+
+    static func json(from response: [ElementResponse]) throws -> String {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+//        let elementsOutput = response.map { Self.elementResponse(from: $0) }
+        
+        let elementsData = try encoder.encode(response)
+        if let json = String(data: elementsData, encoding: .utf8) {
+            return json
+        }
+        return "Couldn't convert response to JSON"
+    }
+
+    static func json(from response: ElementResponse) throws -> String {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+//        let elementOutput = Self.elementResponse(from: response)
+        
+        let elementsData = try encoder.encode(response)
+        if let json = String(data: elementsData, encoding: .utf8) {
+            return json
+        }
+        return "Couldn't convert response to JSON"
+    }
+
+}
