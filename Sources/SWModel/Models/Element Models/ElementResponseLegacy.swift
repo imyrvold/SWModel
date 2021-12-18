c6dc88ac46eb70c61399e204b2e05c0c26048dd4
Ivan C Myrvold <ivan@myrvold.org>
2021-12-18 06:45:32 +0100

    added elementResponse and elementsResponse to ElementResponseLegacy

--- Sources/SWModel/Models/Element Models/ElementResponseLegacy.swift
+++ Sources/SWModel/Models/Element Models/ElementResponseLegacy.swift
@@ -30,52 +30,57 @@ public struct ElementResponseLegacy: Codable {
     }
     
     public init(from decoder: Decoder) throws {
-        print("ElementResponseLegacy init")
         let values = try decoder.container(keyedBy: CodingKeys.self)
         id = try values.decode(ObjectId.self, forKey: .id)
-        print("ElementResponseLegacy init id:", id)
         name = try values.decode(String.self, forKey: .name)
-        print("ElementResponseLegacy init 1")
         type = try values.decode(ElementType.self, forKey: .type)
-        print("ElementResponseLegacy init 2")
         claim = try values.decode(ClaimResponseLegacy.self, forKey: .claim)
-        print("ElementResponseLegacy init 3")
         createdOn = try values.decode(String.self, forKey: .createdOn)
-        print("ElementResponseLegacy init 4")
         updatedOn = try values.decode(String.self, forKey: .updatedOn)
-        print("ElementResponseLegacy init 5")
         deletedOn = try? values.decode(String.self, forKey: .deletedOn)
-        print("ElementResponseLegacy init 6")
         switch type {
         case .tagGroup:
-            print("ElementLegacy init from decoder tagGroup")
             data = try .tagGroup(values.decode(TagGroupData.self, forKey: .data))
         case .tagList:
-            print("ElementLegacy init from decoder tagList")
             data = try .tagList(values.decode(TagListData.self, forKey: .data))
         case .alarmList:
-            print("ElementLegacy init from decoder alarmList")
             data = try .alarmList(values.decode(AlarmListData.self, forKey: .data))
         case .alarmLogList:
-            print("ElementLegacy init from decoder alarmLogList")
             data = try .alarmLogList(values.decode(AlarmLogListData.self, forKey: .data))
         case .iframe:
-            print("ElementLegacy init from decoder iframe")
             data = try .iframe(values.decode(IframeData.self, forKey: .data))
         case .image:
-            print("ElementLegacy init from decoder image")
             data = try .image(values.decode(ImageData.self, forKey: .data))
         case .avPagesCalendar:
-            print("ElementLegacy init from decoder avPagesCalendar")
             data = try .avPagesCalendar(values.decode(AvPagesCalendarData.self, forKey: .data))
         case .avMonthlyCalendar, .advRoundSlider:
-            print("ElementLegacy init from decoder avMonthlyCalendar, .advRoundSlider")
             data = nil
         }
     }
 }
 
 public extension ElementResponseLegacy {
+    struct Element: Encodable {
+        public let id: String
+        public let name: String
+        public let type: ElementType
+        public let data: ElementData?
+        public let claim: String
+        public let createdOn: String
+        public let updatedOn: String
+        public let deletedOn: String?
+    }
+    
+    static func elementResponse(from response: ElementResponseLegacy) -> Element {
+        return Element(id: response.id.hexString, name: response.name, type: response.type, data: response.data, claim: response.claim.id.hexString, createdOn: response.createdOn, updatedOn: response.updatedOn, deletedOn: response.deletedOn)
+    }
+
+    static func elementsResponse(from response: [ElementResponseLegacy]) -> [Element] {
+        response.map { elementResponseLegacy in
+            return Element(id: elementResponseLegacy.id.hexString, name: elementResponseLegacy.name, type: elementResponseLegacy.type, data: elementResponseLegacy.data, claim: elementResponseLegacy.claim.id.hexString, createdOn: elementResponseLegacy.createdOn, updatedOn: elementResponseLegacy.updatedOn, deletedOn: elementResponseLegacy.deletedOn)
+        }
+    }
+
     static func json(from response: [ElementResponseLegacy]) throws -> String {
         let encoder = JSONEncoder()
         encoder.outputFormatting = .prettyPrinted
