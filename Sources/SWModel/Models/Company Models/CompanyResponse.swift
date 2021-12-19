8fe64bdf7cc2480167e71c598057ea4617d38482
Ivan C Myrvold <ivan@myrvold.org>
2021-12-19 13:57:30 +0100

    added static func to CompanyResponse

--- Sources/SWModel/Models/Company Models/CompanyResponse.swift
+++ Sources/SWModel/Models/Company Models/CompanyResponse.swift
@@ -15,4 +15,58 @@ public struct CompanyResponse: Codable {
     public var logo: String?
     public var def: Bool
     public var buildingTabName: String?
+    public var buildingUrl: String?
+    public let createdAt: String
+    public let updatedAt: String
+    public let deletedAt: String?
+}
+
+public extension CompanyResponse {
+    struct Company: Encodable {
+        public let id: String
+        public let title: String
+        public var icons: [CompanyIcon]
+        public let logo: String?
+        public var def: Bool = false
+        public var buildingTabName: String?
+        public var buildingUrl: String?
+        public let createdAt: String
+        public let updatedAt: String
+        public let deletedAt: String?
+    }
+
+    static func companyResponse(from response: CompanyResponse) -> Company {
+        Company(id: response.id.hexString, title: response.title, icons: response.icons, logo: response.logo, def: response.def, buildingTabName: response.buildingTabName, buildingUrl: response.buildingUrl, createdAt: response.createdAt, updatedAt: response.updatedAt, deletedAt: response.deletedAt)
+    }
+
+    static func companiesResponse(from response: [CompanyResponse]) -> [Company] {
+        response.map { companyResponse in
+            return Company(id: companyResponse.id.hexString, title: companyResponse.title, icons: companyResponse.icons, logo: companyResponse.logo, def: companyResponse.def, buildingTabName: companyResponse.buildingTabName, buildingUrl: companyResponse.buildingUrl, createdAt: companyResponse.createdAt, updatedAt: companyResponse.updatedAt, deletedAt: companyResponse.deletedAt)
+        }
+    }
+
+    static func json(from response: [CompanyResponse]) throws -> String {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        let companiesOutput = response.map { Self.companyResponse(from: $0) }
+        
+        let companiesData = try encoder.encode(companiesOutput)
+        if let json = String(data: companiesData, encoding: .utf8) {
+            return json
+        }
+        return "Couldn't convert response to JSON"
+    }
+
+    static func json(from response: CompanyResponse) throws -> String {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        let companyOutput = Self.companyResponse(from: response)
+        
+        let companiesData = try encoder.encode(companyOutput)
+        if let json = String(data: companiesData, encoding: .utf8) {
+            return json
+        }
+        return "Couldn't convert response to JSON"
+    }
+
 }
