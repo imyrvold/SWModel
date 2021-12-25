44e549ce579529c7ae6548bf4a47af5de02b1b27
Ivan C Myrvold <ivan@myrvold.org>
2021-12-25 10:23:39 +0100

    ElementResponseLegacyMinimum don't have claim

--- Sources/SWModel/Models/Element Models/ElementResponseLegacyMinimum.swift
+++ Sources/SWModel/Models/Element Models/ElementResponseLegacyMinimum.swift
@@ -13,13 +13,11 @@ public struct ElementResponseLegacyMinimum: Codable {
     public let name: String
     public let type: ElementType
     public var data: ElementData?
-    public var claim: ClaimResponseLegacy
     
     enum CodingKeys: String, CodingKey {
         case id = "_id"
         case name
         case type
-        case claim
         case data
     }
     
@@ -28,7 +26,6 @@ public struct ElementResponseLegacyMinimum: Codable {
         id = try values.decode(ObjectId.self, forKey: .id)
         name = try values.decode(String.self, forKey: .name)
         type = try values.decode(ElementType.self, forKey: .type)
-        claim = try values.decode(ClaimResponseLegacy.self, forKey: .claim)
         switch type {
         case .tagGroup:
             data = try .tagGroup(values.decode(TagGroupData.self, forKey: .data))
@@ -46,26 +43,6 @@ public struct ElementResponseLegacyMinimum: Codable {
             data = try .avPagesCalendar(values.decode(AvPagesCalendarData.self, forKey: .data))
         case .avMonthlyCalendar, .advRoundSlider:
             data = nil
-        }
-    }
-}
-
-public extension ElementResponseLegacyMinimum {
-    struct Element: Encodable {
-        public let id: String
-        public let name: String
-        public let type: ElementType
-        public let data: ElementData?
-        public let claim: String
-    }
-    
-    static func elementResponse(from response: ElementResponseLegacyMinimum) -> Element {
-        return Element(id: response.id.hexString, name: response.name, type: response.type, data: response.data, claim: response.claim.id.hexString)
-    }
-
-    static func elementsResponse(from response: [ElementResponseLegacyMinimum]) -> [Element] {
-        response.map { elementResponseLegacy in
-            return Element(id: elementResponseLegacy.id.hexString, name: elementResponseLegacy.name, type: elementResponseLegacy.type, data: elementResponseLegacy.data, claim: elementResponseLegacy.claim.id.hexString)
         }
     }
 }
