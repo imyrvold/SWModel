d7be28f0432649c09e5e515779008255c7c447ce
Ivan C Myrvold <ivan@myrvold.org>
2021-12-26 15:53:15 +0100

    fixed SubmenuCreateBody

--- Sources/SWModel/Models/Menu Models/SubmenuCreateBody.swift
+++ Sources/SWModel/Models/Menu Models/SubmenuCreateBody.swift
@@ -10,16 +10,23 @@ import Foundation
 import BSON
 
 public struct SubmenuCreateBody: Encodable {
+    public let id: ObjectId?
     public let name: String
     public let claim: ObjectId
     public let link: String
     public let newTab: Bool?
     
-    public init(name: String, claim: ObjectId, link: String, newTab: Bool?) {
+    public init(id: ObjectId?, name: String, claim: ObjectId, link: String, newTab: Bool?) {
+        self.id = id
         self.name = name
         self.claim = claim
         self.link = link
         self.newTab = newTab
     }
+}
 
+public extension SubmenuCreateBody {
+    func httpBody() throws -> Data {
+        try JSONEncoder().encode(self)
+    }
 }
